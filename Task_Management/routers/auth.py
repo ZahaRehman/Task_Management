e43aec69442947322d410d85dd7d2ba399792a5e
ZahaRehman <zaharehman788@gmail.com>
2025-03-13 00:29:44 +0500

    token file name chnaged

--- Task_Management/routers/auth.py
+++ Task_Management/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from ..import Schemas, database, jwt_token, models 
+from ..import Schemas, database, models, token 
 from sqlalchemy.orm import Session
 from ..hashing import Hash
 
@@ -18,5 +18,5 @@ def login(request: OAuth2PasswordRequestForm=Depends(), db: Session = Depends(da
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Incorrect password")
 
-    access_token = jwt_token.create_access_token(data={"sub": user.email})
+    access_token = token.create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
