62e4488427748b7f7a68b9ef1eaa7bcbe3abafdd
Hassaan Baig <hassaanbaig192@gmail.com>
2025-03-17 21:43:17 +0500

    new changes

--- Task_Management/repository/task.py
+++ Task_Management/repository/task.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException,status, Depends
 from ..hashing import Hash
 
 
-def create_task(project_id: int, request: Schemas.Task, db: Session, current_user: Schemas.User):
+def create_task(project_id: int, request: Schemas.Task, db: Session):
     project = db.query(models.Project).filter(models.Project.id == project_id).first()
     if not project:
         raise HTTPException(status_code=404, detail="Project not found")
